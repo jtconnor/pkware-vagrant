1e2583e7808e649515bfaa8fb176aaa7f4b99c5a
James Connor <jtconnor@gmail.com>
2015-11-20 11:43:21 -0500

    fabric decrypt and encrypt/decrypt dirs.

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # pkware-vagrant
 Runs PKWARE in Vagrant.  Useful to use PKWARE on a Mac.
 
-Initial setup:
+Init virtual environment
 ```
-# virtual environment
 $ virtualenv env
 $ source env/bin/activate
 $ pip install -r ./requirements.txt
 
-# vagrant
+Init vagrant
+```
 $ cp <path to pkware rpm> ./  # `vagrant up` will install the pkware rpm
 $ vagrant up
 $ vagrant ssh
 ```
 
-Encrypt a file:
+Encrypt a file
 ```
-vagrant$ pkzipc -add -passphrase=<p> <path-to-zip> <paths to add to zip> ...
+$ fab encrypt:<glob-pattern>,<password>
 ```
 
-Decrypt a file:
+Decrypt a file
 ```
-local$ fab decrypt:<filename>,<password>
+$ fab decrypt:<filename>,<password>
 ```
--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2.7
-import json
 import os.path
 import uuid
 
@@ -10,17 +9,38 @@ env.hosts = ['127.0.0.1:2222']
 env.key_filename = './.vagrant/machines/default/virtualbox/private_key'
 
 
-def decrypt(filename, password):
-    '''Uses pkware to decrypt `filename` into a unique directory'''
+def create_workdir():
     vagrantdir = os.path.dirname(__file__)
     id = uuid.uuid1()
     workdir = '{vagrantdir}/tmp/{id}'.format(vagrantdir=vagrantdir, id=id)
     local('mkdir {workdir}'.format(workdir=workdir))
-    local('cp {filename} {workdir}'.format(filename=filename, workdir=workdir))
+    local('mkdir {workdir}/encrypted'.format(workdir=workdir))
+    local('mkdir {workdir}/decrypted'.format(workdir=workdir))
+    return workdir, id
+
+
+def encrypt(glob, password, name='encrypted'):
+    workdir, id = create_workdir()
+    local('cp {glob} {workdir}/decrypted'.format(
+        glob=glob, workdir=workdir))
+    run('''\
+    pkzipc -add -passphrase="{password}" \
+      /vagrant/tmp/{id}/encrypted/{name}.zip \
+      /vagrant/tmp/{id}/decrypted/*
+    '''.format(password=password, id=id, name=name))
+    print 'Encrypted to {workdir}/encrypted/{name}.zip'.format(
+        workdir=workdir, name=name)
+
+
+def decrypt(filename, password):
+    '''Uses pkware to decrypt `filename` into a unique directory'''
+    workdir, id = create_workdir()
+    local('cp {filename} {workdir}/encrypted'.format(
+        filename=filename, workdir=workdir))
     basename = os.path.basename(filename)
     run('''\
     pkzipc -extract -passphrase="{password}" \
-      /vagrant/tmp/{id}/{basename} \
-      /vagrant/tmp/{id}
+      /vagrant/tmp/{id}/encrypted/{basename} \
+      /vagrant/tmp/{id}/decrypted
     '''.format(password=password, id=id, basename=basename))
-    print 'Decrypted into {workdir}'.format(workdir=workdir)
+    print 'Decrypted into {workdir}/decrypted'.format(workdir=workdir)
