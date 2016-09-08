136eca61b801a229fcf79e4e0f309bfe705efd5f
James Connor <jtconnor@gmail.com>
2016-09-08 11:08:26 -0400

    Tweak to handle file or directory input.

--- fabfile.py
+++ fabfile.py
@@ -1,46 +1,83 @@
 #!/usr/bin/env python2.7
+'''Encrypts and decrypts files using pkware in a VM.'''
 import datetime
+import fabric.api as fab
 import os.path
-
-from fabric.api import *
-
-env.user = 'vagrant'
-env.hosts = ['127.0.0.1:2222']
-# from `vagrant ssh-config`
-env.key_filename = './.vagrant/machines/default/virtualbox/private_key'
+import subprocess
 
 
-def create_workdir():
-    vagrantdir = os.path.dirname(__file__)
-    id = datetime.datetime.now().isoformat()
-    workdir = '{vagrantdir}/work/{id}'.format(vagrantdir=vagrantdir, id=id)
-    local('mkdir -p {workdir}/encrypted'.format(workdir=workdir))
-    local('mkdir {workdir}/decrypted'.format(workdir=workdir))
-    return workdir, id
+ssh_config = {}
+for line in subprocess.check_output(['vagrant', 'ssh-config']).split('\n'):
+    line = line.strip()
+    if not line:
+        continue
+    key, value = line.split(' ')
+    ssh_config[key] = value
+
+fab.env.user = ssh_config['User']
+fab.env.hosts = ['{}:{}'.format(ssh_config['HostName'], ssh_config['Port'])]
+fab.env.key_filename = ssh_config['IdentityFile']
 
 
-def encrypt(glob, password, name='encrypted.zip'):
-    workdir, id = create_workdir()
-    local('cp "{glob}" {workdir}/decrypted'.format(
-        glob=glob, workdir=workdir))
-    run('''\
-    pkzipc -add -passphrase="{password}" \
-      /vagrant/work/{id}/encrypted/{name} \
-      /vagrant/work/{id}/decrypted/*
-    '''.format(password=password, id=id, name=name))
-    print 'Encrypted to {workdir}/encrypted/{name}'.format(
-        workdir=workdir, name=name)
+def _create_tmpdirs():
+    tmpdir = '/tmp/pkware-vagrant'
+    fab.run('rm -rf {}'.format(tmpdir))
+    encrypted_dir = tmpdir + '/e'
+    fab.run('mkdir -p {}'.format(encrypted_dir))
+    decrypted_dir = tmpdir + '/d'
+    fab.run('mkdir {}'.format(decrypted_dir))
+    return tmpdir, encrypted_dir, decrypted_dir
 
 
-def decrypt(filename, password):
-    '''Uses pkware to decrypt `filename` into a unique directory'''
-    workdir, id = create_workdir()
-    local('cp "{filename}" {workdir}/encrypted'.format(
-        filename=filename, workdir=workdir))
-    basename = os.path.basename(filename)
-    run('''\
-    pkzipc -extract -passphrase="{password}" \
-      /vagrant/work/{id}/encrypted/* \
-      /vagrant/work/{id}/decrypted
-    '''.format(password=password, id=id, basename=basename))
-    print 'Decrypted into {workdir}/decrypted'.format(workdir=workdir)
+def encrypt(file_or_directory, password, output_filename=None):
+    tmpdir, encrypted_dir, decrypted_dir = _create_tmpdirs()
+
+    if os.path.isdir(file_or_directory):
+        fab.put(file_or_directory + '/*', decrypted_dir)
+    else:
+        fab.put(file_or_directory, decrypted_dir)
+
+    fab.run('''\
+    pkzipc -add -passphrase='{password}' \
+      {encrypted_dir}/out.zip {decrypted_dir}/*
+    '''.format(
+        password=password, encrypted_dir=encrypted_dir,
+        decrypted_dir=decrypted_dir))
+
+    if output_filename is None:
+        dirname = os.path.dirname(file_or_directory)
+        basename = os.path.basename(file_or_directory)
+        output_filename = os.path.join(
+            dirname,
+            '{}-encrypted.zip'.format(basename))
+
+    fab.get('{}/out.zip'.format(encrypted_dir), output_filename)
+    print 'Encrypted to {}'.format(output_filename)
+
+    fab.run('rm -rf {}'.format(tmpdir))
+
+
+def decrypt(filename, password, output_dir=None):
+    tmpdir, encrypted_dir, decrypted_dir = _create_tmpdirs()
+
+    fab.put(filename, encrypted_dir)
+
+    fab.run('''\
+    pkzipc -extract -passphrase='{password}' \
+      {encrypted_dir}/* {decrypted_dir}
+    '''.format(
+        password=password, encrypted_dir=encrypted_dir,
+        decrypted_dir=decrypted_dir))
+
+    if output_dir is None:
+        dirname = os.path.dirname(filename)
+        basename = os.path.basename(filename)
+        output_dir = os.path.join(
+            dirname,
+            '{}-decrypted'.format(basename))
+        fab.local('mkdir {}'.format(output_dir))
+
+    fab.get(decrypted_dir + '/*', output_dir)
+    print 'Decrypted to {}'.format(output_dir)
+
+    fab.run('rm -rf {}'.format(tmpdir))
